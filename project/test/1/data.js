cbeb74485d1c4de94297289a7e7ba2fb09296c85
EricHsu <aerchit@gmail.com>
2019-08-13 10:23:53 +0800

    marzipano 1

---
+++ project/test/1/data.js
@@ -0,0 +1,164 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bjla_south-360_20181213_eric_finaljpg",
+      "name": "BJLA通惠河_south 360_20181213_Eric_Final.jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.544700993502449,
+        "pitch": -0.11702337414734032,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.586288247898577,
+          "pitch": 0.13915825209166854,
+          "rotation": 12.566370614359176,
+          "target": "1-bjla_north-360_20181213_eric_finaljpg"
+        },
+        {
+          "yaw": 2.5860360363141357,
+          "pitch": 0.03817358253527203,
+          "rotation": 0.7853981633974483,
+          "target": "2-bjla_east-360_20181213_william_final"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.659350025427365,
+          "pitch": -0.23098399024813254,
+          "title": "測試測試",
+          "text": "測試<div>測試</div><div>測試</div>"
+        },
+        {
+          "yaw": -1.1361582429766628,
+          "pitch": -0.2225010448784026,
+          "title": "POI",
+          "text": "POIPOIPOIPOI<br>測試測試測試<div><br></div><div>測試測試測試<br></div><div>測試測試測試測試測試測試<br></div><div><br></div><div>測試測試測試<br></div><div><br></div><div>測試測試測試測試測試測試<br></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-bjla_north-360_20181213_eric_finaljpg",
+      "name": "BJLA通惠河_north 360_20181213_Eric_Final.jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.580887715888334,
+        "pitch": -0.22792552194975002,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5809202635133843,
+          "pitch": 0.1168112823409082,
+          "rotation": 0,
+          "target": "0-bjla_south-360_20181213_eric_finaljpg"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5331269066641369,
+          "pitch": -0.324237417123344,
+          "title": "神木神木神木神木神木神木神木神木神木",
+          "text": "字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試字數測試"
+        },
+        {
+          "yaw": 1.6124413921301697,
+          "pitch": -0.2524005364341626,
+          "title": "Test",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">Test</span><span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">Test</span><span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">Test</span><span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">Test</span><br>"
+        }
+      ]
+    },
+    {
+      "id": "2-bjla_east-360_20181213_william_final",
+      "name": "BJLA通惠河_east 360_20181213_William_Final",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03606192926122809,
+          "pitch": -0.17400811404368355,
+          "rotation": 0,
+          "target": "0-bjla_south-360_20181213_eric_finaljpg"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.874115444066314,
+          "pitch": -0.2595832920507597,
+          "title": "宇光的遺作",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "WebVR 測試",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
