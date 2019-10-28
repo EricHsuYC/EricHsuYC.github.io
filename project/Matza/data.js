0daebf535d6140267c2d52a41e2105002725f1ba
Eric_hsu1 <Eric.hsu1@aecom.com>
2019-10-28 16:44:14 +0800

    add Matzu non-VR

---
+++ project/Matza/data.js
@@ -0,0 +1,257 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-introduction",
+      "name": "Introduction",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -3.031095453550959,
+        "pitch": 0.08731980649831073,
+        "fov": 1.3487598040662003
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0916837322309494,
+          "pitch": 0.6762084223247076,
+          "rotation": 3.141592653589793,
+          "target": "1-water-recycling-center_before"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-water-recycling-center_before",
+      "name": "Water Recycling Center_Before",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -3.0333670870386484,
+        "pitch": 0.08985372585268081,
+        "fov": 1.3487598040662003
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0745706050385575,
+          "pitch": 0.39002752897003035,
+          "rotation": 3.141592653589793,
+          "target": "2-water-recycling-center_after"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-water-recycling-center_after",
+      "name": "Water Recycling Center_After",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.6938471418436407,
+        "pitch": -0.0258186782007499,
+        "fov": 1.3487598040662003
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.281220183013776,
+          "pitch": -0.14192695255994714,
+          "rotation": 0.7853981633974483,
+          "target": "3-qingshui-wetlands_before"
+        },
+        {
+          "yaw": 2.910322068999495,
+          "pitch": 0.6785876642023734,
+          "rotation": 3.141592653589793,
+          "target": "1-water-recycling-center_before"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.835888446278453,
+          "pitch": -0.28581945909729356,
+          "title": "<span style=\"font-size: 13px;\">New Water Recycling Center</span><br>",
+          "text": "<div>The newly built water recycling center integrated existing Fuqing plant, it increases wastewater treatment capacity (MBR), and provides water supply for wetland and surrounding facilities. It is also used as a resting point for the coastal walk.</div>"
+        },
+        {
+          "yaw": 0.3680728363956707,
+          "pitch": 0.20043098648357827,
+          "title": "Improvement of the costal walk",
+          "text": "Improve the material of the trails and walkway to enhance safety and durability, making it easier to maintain."
+        },
+        {
+          "yaw": 1.3331607651344282,
+          "pitch": 0.46959502285099397,
+          "title": "Fuqing Coastal Walk",
+          "text": "Integrate the existing 2km coastal walk, connect the dock area with the wetland, and create a special waterfront."
+        }
+      ]
+    },
+    {
+      "id": "3-qingshui-wetlands_before",
+      "name": "Qingshui Wetlands_Before",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.592215477688761,
+        "pitch": 0.005327204575760192,
+        "fov": 1.3487598040662003
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.532319902916136,
+          "pitch": -0.1893281524613286,
+          "rotation": 3.141592653589793,
+          "target": "4-qingshui-wetlands_after"
+        },
+        {
+          "yaw": 2.893831533505784,
+          "pitch": 0.011480836343961798,
+          "rotation": 5.497787143782138,
+          "target": "1-water-recycling-center_before"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-qingshui-wetlands_after",
+      "name": "Qingshui Wetlands_After",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.5364820028201756,
+        "pitch": 0.05481442614979848,
+        "fov": 1.3487598040662003
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5803937686595937,
+          "pitch": -0.19483523044418405,
+          "rotation": 3.141592653589793,
+          "target": "3-qingshui-wetlands_before"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.3023328130556244,
+          "pitch": 0.12639931834664608,
+          "title": "Water Education Center and Observation Deck",
+          "text": "<div>This is the first environmental education center in Matsu for demonstrating water resources systems and ecology in Nanxun area.</div>"
+        },
+        {
+          "yaw": -1.963102572585104,
+          "pitch": 0.33330222078835803,
+          "title": "Title",
+          "text": "A tern in the family Laridae, closely related to the Sandwich tern."
+        },
+        {
+          "yaw": -0.8027071500131306,
+          "pitch": 0.3420489117527019,
+          "title": "Wetland Conservation",
+          "text": "Strengthen the conservation of Qingshui national wetland by improving water quality and planting to enhance habitat environment for wild animals."
+        },
+        {
+          "yaw": 0.8093015814794846,
+          "pitch": 0.3627513583982864,
+          "title": "Reinforcements of the Walkway",
+          "text": "Reinforce surrounding walkways to increase safety and looking"
+        }
+      ]
+    }
+  ],
+  "name": "Matzu Water and Environmental Design VR",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
