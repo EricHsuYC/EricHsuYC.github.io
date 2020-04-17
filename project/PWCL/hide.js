aabe29994fd15391898584dec7838ec0d87197f2
Eric_hsu1 <Eric.hsu1@aecom.com>
2020-04-17 18:45:10 +0800

    Update hide.js

--- project/PWCL/hide.js
+++ project/PWCL/hide.js
@@ -2,3 +2,13 @@ document.addEventListener('contextmenu', event => event.preventDefault());
 
 var list = document.getElementsByTagName("UL")[0];
 list.style.display = "none";
+
+
+function rightclick() {
+    var rightclick;
+    var e = window.event;
+    if (e.which) rightclick = (e.which == 3);
+    else if (e.button) rightclick = (e.button == 2);
+    alert("Right mouse button " + (isRightMB ? "" : " was not") + "clicked!");
+}
+
