b5c4de06c2edfd97b965d948487348c1f95e7bde
Eric_hsu1 <Eric.hsu1@aecom.com>
2020-04-20 14:21:17 +0800

    PWCL

--- project/PWCL/hide.js
+++ project/PWCL/hide.js
@@ -1,16 +1,16 @@
-document.addEventListener('contextmenu', event => event.preventDefault());
+//document.addEventListener('contextmenu', event => event.preventDefault());
 
 var list = document.getElementsByTagName("UL")[0];
 list.style.display = "none";
 
 
-// function rightclick() {
-//     var rightclick;
-//     var e = window.event;
-//     if (e.which) rightclick = (e.which == 3);
-//     else if (e.button) rightclick = (e.button == 2);
-//     alert("Right mouse button " + (isRightMB ? "" : " was not") + "clicked!");
-// }
+function rightclick() {
+    var rightclick;
+    var e = window.event;
+    if (e.which) rightclick = (e.which == 3);
+    else if (e.button) rightclick = (e.button == 2);
+    alert("Right mouse button " + (isRightMB ? "" : " was not") + "clicked!");
+}
 
 document.body.onclick = function (e) {
     var isRightMB;
